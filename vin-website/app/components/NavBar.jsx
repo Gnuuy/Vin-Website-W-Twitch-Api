2ff11740aedef8d15fbbc6621303fd55798fc09b
Elphelt <>
2023-08-20 18:29:09 +0200

    Moved picture, WIP NavBar

--- vin-website/app/components/NavBar.jsx
+++ vin-website/app/components/NavBar.jsx
@@ -1,8 +1,11 @@
-export default function NavBar()
-{
+import Link from 'next/link'
+
+export default function NavBar(){
     return (
         <nav>
             <h1>Nav Bar</h1>
+            <Link href="/">Home</Link>
+            <Link href="/">About</Link>
         </nav>
     )
 }
